eef99ece788816fedb2a2dd93fc61eca9ee99647
yash <ky14@iitbbs.ac.in>
2024-12-22 20:01:14 +0530

    made performance optimisation

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import React, {lazy, Suspense} from 'react';
 import  LandingPage  from './components/Landing'
-import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { RecoilRoot, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 import { countAtom } from './store/atoms/count';
 const  Navbar = lazy(()=>import ('./components/Navbar'));
 const Dashboard=lazy(()=>import ('./components/Dashboard'))
@@ -52,17 +52,18 @@ function CountRender(){
 
 function Buttons(){
 
-  const [count,setCount]=useRecoilState(countAtom)
+  // const [setCount]=useRecoilState(countAtom)
+  const setCount = useSetRecoilState(countAtom)
 
 return(
   <div>
     <button onClick={()=>{
-      setCount(count+1)
+      setCount(count => count+1)
     }}>
       Increase
     </button>
     <button onClick={()=>{
-      setCount(count-1)
+      setCount(count => count-1)
     }}>
       Decrease
     </button>

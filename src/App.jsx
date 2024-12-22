60f8b89e21fd76767382eaaa6e11b06ec9b67718
yash <ky14@iitbbs.ac.in>
2024-12-22 11:44:10 +0530

    implemented and worked with context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,66 @@
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import React, {lazy} from 'react';
+import React, {lazy, Suspense, useContext, useState} from 'react';
 import  LandingPage  from './components/Landing'
-import Navbar from lazy(()=>import ('./components/Navbar'));
+import { CountContext } from './context';
+const  Navbar = lazy(()=>import ('./components/Navbar'));
 const Dashboard=lazy(()=>import ('./components/Dashboard'))
 
 function App() {
-
-  
-
+  const [count,setCount]=useState(0);
   return (
     <div>
-
-      
       <BrowserRouter> 
       <Navbar/>
         <Routes>
-          <Route path='/dashboard' element={<Dashboard />}></Route>
-          <Route path='/' element={<LandingPage />}></Route>
+          <Route path='/dashboard' element={<Suspense fallback={'loading...'}><Dashboard /></Suspense>}></Route>
+          <Route path='/' element={<Suspense fallback={'loading...'}><LandingPage /></Suspense>}></Route>
         </Routes>
       </BrowserRouter>
-    </div>
 
+      <CountContext.Provider value={{count, setCount}}>
+      <Count count={count} setCount={setCount}/>
+      </CountContext.Provider>
+      
+      {/* <Buttons setCount={setCount}/> */}
+    </div>
   )
+}
+
+function Count({count, setCount}){
+  return(
+    <div>
+      <CountRender/>
+      <Buttons count={count} setCount={setCount}/>
+    </div>
+  )
+}
+
+function CountRender(){
+  const count = useContext(CountContext);
+  return(
+    <div>
+      {count}
+    </div>
+  )
+}
+
+function Buttons({setCount}){
+  const count = useContext(CountContext);
+return(
+  <div>
+    <button onClick={()=>{
+      setCount(count+1)
+    }}>
+      Increase
+    </button>
+    <button onClick={()=>{
+      setCount(count-1)
+    }}>
+      Decrease
+    </button>
+  </div>
+)
 }
 
 export default App
